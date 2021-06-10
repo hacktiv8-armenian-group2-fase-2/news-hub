c3fc18ea16e2537f99e329036017e5b33c6d98e1
budigk <budigk@gmail.com>
2021-06-10 10:08:19 +0700

    Adding Login-Google

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const { comparePassword } = require('../helpers/bcrypt.js')
 const {user} = require('../models/index.js')
 const { generateToken } = require('../helpers/jwt.js')
+const {OAuth2Client} = require('google-auth-library')
 
 class UserController{
     static register(req, res, next) {
@@ -63,6 +64,49 @@ class UserController{
         })
         .catch(err => {
             next(err)
+        })
+    }
+
+    static loginGoogle(req, res, next){
+        const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+        const {id_token_google } =req.body;
+        let emailUser = "";
+
+        client.verifyIdToken({
+            idToken: id_token_google,
+            audience: process.env.GOOGLE_CLIENT_ID
+        })
+        .then(ticket => {
+            const payload = ticket.getPayload();
+            const {email} = payload
+            emailUser = email;
+
+            return users.findOne({
+                where: {email: emailUser}
+            })
+        }).then(user => {
+            if (!user) {
+                console.log("create user")
+                return users.create({
+                    email: emailUser,
+                    password: String(Math.random()) + String(Math.random())
+                })
+            } else {
+                console.log("DONE user")
+                return {
+                    id: user.id,
+                    email: user.email
+                }
+            }
+        })
+        .then(user => {
+            console.log("user ==>", user)
+            const token = generateToken({
+                id: user.id,
+                email: user.email
+            })
+
+            res.status(201).json({access_token: token})
         })
     }
 }
